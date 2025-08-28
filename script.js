876803ca7af2bba59708c495a6345ad9061c95aa
Willian Tomaselli <153656673+williantomaselli@users.noreply.github.com>
2025-08-28 00:48:17 -0300

    infinite reload bug fixed

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ const menu = document.getElementById("menu");
 abrir.addEventListener("click", () => menu.classList.add("show"));
 fechar.addEventListener("click", () => menu.classList.remove("show"));
 
-var rellax = new Rellax('.rellax', {
-});
+var rellax = new Rellax(".rellax", {});
 
 window.addEventListener("load", function () {
   rellax && typeof rellax.refresh === "function" && rellax.refresh();
 });
-
-
 
 // contato whatsapp
 document.getElementById("sendBtn").addEventListener("click", function () {
@@ -43,17 +40,21 @@ document.getElementById("sendBtn").addEventListener("click", function () {
   let whatsappMessage = `Olá, eu sou ${name}, vim pelo site e gostaria de falar: ${message}`;
 
   // Abre WhatsApp
-  let whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+  let whatsappURL = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(
+    whatsappMessage
+  )}`;
   window.open(whatsappURL, "_blank");
 });
 
-
 document.addEventListener("DOMContentLoaded", () => {
   const larguraMaxMobile = 768;
+  const atual = window.location.pathname.split("/").pop(); // pega o nome do arquivo atual
 
-  if (window.innerWidth > larguraMaxMobile) {
+  if (window.innerWidth > larguraMaxMobile && atual !== "working.html") {
     window.location.href = "./working.html";
-  }else{
+  }
+
+  if (window.innerWidth <= larguraMaxMobile && atual !== "index.html") {
     window.location.href = "./index.html";
   }
 });
